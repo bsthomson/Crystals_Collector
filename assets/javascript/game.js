078582a8315463abe23af6e61339e2cd663ab3cb
bsthomson <thomson.bs@gmail.com>
2018-06-07 18:03:47 -0500

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-var targetNumber=55;
+var targetNumber=53;
 
 $("#number-to-guess").text(targetNumber);
 
@@ -8,10 +8,20 @@ var counter=0;
 
 var numberOptions=[10,5,3,7];
 
+for (var i=0; i < numberOptions.length; i++) {
+    var imageCrystal = $("<img>");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("src", "assets/images/crystal.jpg");
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+    $("#crystals").append(imageCrystal);
+}
 
 $(".crystal-image").on("click", function() {
-    counter += increment;
-    alert("Your new score is " + counter);
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue=parseInt(crystalValue);
+    counter += crystalValue;
+    alert("New Score " + counter);
+
     if (counter === targetNumber) {
         alert("You Win!");
     }
